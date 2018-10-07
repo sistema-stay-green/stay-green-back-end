e0f3fec4a0f6cd234ef82b072e2062d29e438639
Daniel Franco Lopes <dandanielfranco@yahoo.com.br>
2018-10-07 02:02:59 -0300

    Define a anotação para tabelas

---
+++ StayGreen/src/main/java/br/cefetmg/staygreen/annotation/Tabela.java
@@ -0,0 +1,28 @@
+/*
+ * Sistema de Agronegocio :: Stay Green
+ * CEFET-MG
+ * INF-2A 2018
+ */
+package br.cefetmg.staygreen.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Indica que uma classe representa uma tabela. Dessa forma, classes como
+ * a {@link br.cefetmg.staygreen.util.SQL} poderão manipular os dados dos
+ * objetos da classe anotada para realizar inserção e atualização de
+ * registros na respectiva tabela.
+ * 
+ * @author Daniel
+ * @version 1.0
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Tabela {
+    
+    public String value();
+    
+}
