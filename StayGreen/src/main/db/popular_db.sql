ee7ee706a05288fa2d8ed6c617d32963ee8317ed
Daniel Franco Lopes <dandanielfranco@yahoo.com.br>
2018-10-09 17:29:04 -0300

    Adiciona os scripts SQL necessários do projeto

---
+++ StayGreen/src/main/db/popular_db.sql
@@ -0,0 +1,8 @@
+/* 
+ * Sistema de Agronegocio :: Stay Green
+ * CEFET-MG
+ * INF-2A 2018
+ */
+
+-- Exemplo: INSERT INTO tabela (...) VALUES (...), (...), ..., (...);
+
---
+++ StayGreen/src/main/db/schema_db.sql
@@ -0,0 +1,19 @@
+/* 
+ * Sistema de Agronegocio :: Stay Green
+ * CEFET-MG
+ * INF-2A 2018
+ */
+
+-- ----------------------------
+-- Criação do banco de dados:
+-- ----------------------------
+
+DROP DATABASE IF EXISTS `staygreen`;
+
+CREATE DATABASE `staygreen` DEFAULT CHARACTER SET utf8;
+
+-- ----------------------
+-- Criação das tabelas:
+-- ----------------------
+
+-- Exemplo: CREATE TABLE tabela (...) DEFAULT CHARACTER SET utf8;
