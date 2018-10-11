1bab14d3ab7f4000137e355016a619067a5c2989
Mei Fagundes <MeiFagundes@outlook.com>
2018-10-10 22:37:45 -0300

    Add 'patrimonio' table to schema/popular SQL scripts

--- StayGreen/src/main/db/popular_db.sql
+++ StayGreen/src/main/db/popular_db.sql
@@ -6,3 +6,9 @@
 
 -- Exemplo: INSERT INTO tabela (...) VALUES (...), (...), ..., (...);
 
+-- Tabela Patrimonio
+
+INSERT INTO `patrimonio` (`Id`, `Nome`, `Tipo`, `Descrição`, `Status`, `Índice de Depreciação`, `Valor da Compra`, `Valor Atual`, `Data da Compra`, `Data da Saída`, `Dada da Baixa`)
+VALUES (NULL, 'Trator', 'Máquina', 'Uma máquina que ajuda na colheita do café.', 'VENDIDO', '15', '5000', '4800', '2018-10-01', '2018-10-03', '2018-10-02');
+
+-- Tabela ...
--- StayGreen/src/main/db/schema_db.sql
+++ StayGreen/src/main/db/schema_db.sql
@@ -12,8 +12,35 @@ DROP DATABASE IF EXISTS `staygreen`;
 
 CREATE DATABASE `staygreen` DEFAULT CHARACTER SET utf8;
 
+USE `staygreen`;
+
 -- ----------------------
 -- Criação das tabelas:
 -- ----------------------
+
+-- Tabela Patrimonio
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+DROP TABLE IF EXISTS `patrimonio`;
+CREATE TABLE IF NOT EXISTS `patrimonio` (
+  `Id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `Nome` varchar(30) DEFAULT NULL,
+  `Tipo` varchar(30) DEFAULT NULL,
+  `Descrição` text,
+  `Status` enum('VENDIDO','ALUGADO','EM_POSSE','DESCARTADO') DEFAULT NULL,
+  `Índice de Depreciação` double DEFAULT NULL,
+  `Valor da Compra` double DEFAULT NULL,
+  `Valor Atual` double DEFAULT NULL,
+  `Data da Compra` date DEFAULT NULL,
+  `Data da Saída` date DEFAULT NULL,
+  `Dada da Baixa` date DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+
+-- Tabela ...
 
 -- Exemplo: CREATE TABLE tabela (...) DEFAULT CHARACTER SET utf8;
