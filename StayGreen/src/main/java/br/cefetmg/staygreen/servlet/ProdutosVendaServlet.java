f225325811120fbe70fca26936a440847683316a
Paulo Vitor <pvitoreu@gmail.com>
2018-10-21 02:42:40 -0300

    Cria a classe ProdutosVendaServlet
    Cria as classes para recuperar os produtos

---
+++ StayGreen/src/main/java/br/cefetmg/staygreen/servlet/ProdutosVendaServlet.java
@@ -0,0 +1,104 @@
+/*
+ * Sistema de Agronegocio :: Stay Green
+ * CEFET-MG
+ * INF-2A 2018
+ */
+package br.cefetmg.staygreen.servlet;
+
+import br.cefetmg.staygreen.table.Produto;
+import br.cefetmg.staygreen.util.JSON;
+import br.cefetmg.staygreen.util.SQL;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet que retorna uma string em formato JSON contendo dados dos produtos registrados
+ * @version 1.0
+ * @author Paulo Vitor
+ */
+@WebServlet(name = "ProdutosVendaServlet", urlPatterns = {"/ProdutosVendaServlet"})
+public class ProdutosVendaServlet extends HttpServlet {
+    
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     * @throws java.sql.SQLException
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException {
+        response.setContentType("application/json;charset=UTF-8");
+        
+        ResultSet produto = SQL.query("SELECT * FROM " + SQL.getNomeTabela(Produto.class));
+        String resultado = "[";
+        while(produto.next())
+                 resultado += JSON.stringify(new Produto(produto)) + (produto.isLast()?"":",");
+        resultado += "]";
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println(resultado);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(ProdutosVendaServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(ProdutosVendaServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ StayGreen/src/main/java/br/cefetmg/staygreen/table/Produto.java
@@ -0,0 +1,147 @@
+/*
+ * Sistema de Agronegocio :: Stay Green
+ * CEFET-MG
+ * INF-2A 2018
+ */
+
+package br.cefetmg.staygreen.table;
+
+import br.cefetmg.staygreen.annotation.Id;
+import br.cefetmg.staygreen.annotation.Tabela;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Classe relacionada à tabela de produtos (temporária devido a possíveis mudanças)
+ * @author Paulo Vitor
+ * @version 1.0
+ */
+@Tabela("produtos")
+public class Produto {
+    @Id
+    private Long id;
+    private String nome;
+    private String descricao;
+    private Double valorUniCompra;
+    private Double valorUniVenda;
+    private Double pontoAviso;
+    private Double estoque;
+    private Double unidadeVenda;
+    
+    /**
+     * Cria um Produto
+     * @param id identificação
+     * @param nome nome do produto
+     * @param descricao descrição do produto
+     * @param valorUniCompra valor de compra por unidade
+     * @param valorUniVenda valor de venda por unidade
+     * @param pontoAviso ponto de aviso do estoque
+     * @param estoque estoque disponível
+     * @param unidadeVenda unidade de venda
+     */
+    public Produto(Long id, String nome, String descricao, Double valorUniCompra, Double valorUniVenda, Double pontoAviso, Double estoque, Double unidadeVenda) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.valorUniCompra = valorUniCompra;
+        this.valorUniVenda = valorUniVenda;
+        this.pontoAviso = pontoAviso;
+        this.estoque = estoque;
+        this.unidadeVenda = unidadeVenda;
+    }
+
+    /**
+     * Cria um produto sem id
+     * @param nome nome do produto
+     * @param descricao descrição do produto
+     * @param valorUniCompra valor de compra por unidade
+     * @param valorUniVenda valor de venda por unidade
+     * @param pontoAviso ponto de aviso do estoque
+     * @param estoque estoque disponível
+     * @param unidadeVenda unidade de venda
+     */
+    public Produto(String nome, String descricao, Double valorUniCompra, Double valorUniVenda, Double pontoAviso, Double estoque, Double unidadeVenda) {
+        this(null, nome, descricao, valorUniCompra, valorUniVenda, pontoAviso, estoque, unidadeVenda);
+    }
+    
+    public Produto(ResultSet resultado) throws SQLException{
+        this(resultado.getLong("id"), resultado.getString("nome"), resultado.getString("descricao"), resultado.getDouble("valorUniCompra"),
+                resultado.getDouble("valorUniVenda"), resultado.getDouble("pontoAviso"), resultado.getDouble("estoque"), resultado.getDouble("unidadeVenda"));
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getValorUniCompra() {
+        return valorUniCompra;
+    }
+
+    public void setValorUniCompra(Double valorUniCompra) {
+        this.valorUniCompra = valorUniCompra;
+    }
+
+    public Double getValorUniVenda() {
+        return valorUniVenda;
+    }
+
+    public void setValorUniVenda(Double valorUniVenda) {
+        this.valorUniVenda = valorUniVenda;
+    }
+
+    public Double getPontoAviso() {
+        return pontoAviso;
+    }
+
+    public void setPontoAviso(Double pontoAviso) {
+        this.pontoAviso = pontoAviso;
+    }
+
+    public Double getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(Double estoque) {
+        this.estoque = estoque;
+    }
+
+    public Double getUnidadeVenda() {
+        return unidadeVenda;
+    }
+
+    public void setUnidadeVenda(Double unidadeVenda) {
+        this.unidadeVenda = unidadeVenda;
+    }
+
+    @Override
+    public String toString() {
+        return "Id: " + id + "\nNome: " + nome + "\nDescrição: " + descricao + "\nValor de Compra: " + valorUniCompra
+            + "\nValor de Venda: " + valorUniVenda 
+            + "\nPonto de aviso: " + pontoAviso 
+            + "\nEstoque disponível: " +estoque  
+            + "\nUnidade de Venda: " + unidadeVenda;
+    }
+    
+    
+    
+}
